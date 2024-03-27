498f409b6d48a7361961b9df2299202076a8f673
Mohammad Shahwaz <shahwazsid777@gmail.com>
2024-03-27 17:20:19 +0530

    Adding project on github

---
+++ src/components/Instructors.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { WavyBackground } from "./ui/wavy-background";
+import { AnimatedTooltip } from "./ui/animated-tooltip";
+
+const instructors = [
+  {
+    id: 1,
+    name: "Elena Briggs",
+    designation: "Vocal Coach",
+    image:
+      "https://images.unsplash.com/photo-1664575602554-2087b04935a5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    name: "Marcus Reid",
+    designation: "Guitar Instructor",
+    image:
+      "https://images.unsplash.com/photo-1619545010907-ca2217d296ce?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    name: "Julia Zhang",
+    designation: "Piano Teacher",
+    image:
+      "https://images.unsplash.com/photo-1609081376241-49a678b55afb?q=80&w=1826&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    name: "Andre Gomez",
+    designation: "Drumming Expert",
+    image:
+      "https://images.unsplash.com/photo-1528900403525-dc523d4f18d6?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+function Instructors() {
+  return (
+    <div className="relative h-[40rem] overflow-hidden flex items-center justify-center">
+      <WavyBackground className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center h-full">
+        <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">
+          Meet Our Instructors
+        </h2>
+        <p className="text-base md:text-lg text-white text-center mb-4">
+          Discover the talented professionals who will guide your musical
+          journey
+        </p>
+        <div className="flex flex-row items-center justify-center mb-10 w-full">
+          <AnimatedTooltip items={instructors} />
+        </div>
+      </WavyBackground>
+    </div>
+  );
+}
+
+export default Instructors;
